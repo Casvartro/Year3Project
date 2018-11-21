df868685678348822db8e091e60640db15885412
Cieran Brazier <c.brazier@student.liverpool.ac.uk>
2018-11-21 01:16:26 +0000

    Player Development Day 1
    Added Crosshair from Unity Asset Store on canvas.
    Removed Rigidbody in place of a CharacterController as the player will not have realistic physics.
    Allowed camera rotation for the player based on mouse movement.
    Added in functionality for moving in the appropriate direction based on camera rotation.
    
    ---TO DO STILL---
    When looking directly up still move forward rather than move directly into the air, same with backwards rather than down.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
-	private Rigidbody playerRb;
+	public float movementSpeed;
+	public float gravity = 20.0f;
+
+	public float cameraSpeed;
+	private float rotY = 0.0f;
+	private float rotX = 0.0f;
+
+	private Vector3 movementDir;
+	private CharacterController player;
 
 	// Use this for initialization
 	void Start () {
-		
-		playerRb = GetComponent<Rigidbody> ();
-
+		player = this.GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		//Updates camera rotation based on player input from mouse movement
+		rotX += cameraSpeed * Input.GetAxis("Mouse X");
+		rotY -= cameraSpeed * Input.GetAxis("Mouse Y");
+
+		if (rotY > 90.0f) {
+			rotY = 90.0f;
+		} else if (rotY < -90.0f) {
+			rotY = -90.0f;
+		}
+
+		//Euler angles used to overwrite the rotation value each time it changes to its new current position.
+		transform.eulerAngles = new Vector3(rotY, rotX, 0.0f);
+
 	}
 
 
 	void FixedUpdate () {
-		
-		//Updates based on player input.
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
 
-		//Updates current movement and speed.
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		playerRb.velocity = movement * speed;
-	
+		if (player.isGrounded) {
+			//Updates based on player input.
+			movementDir = Vector3.zero;
+			movementDir.x = Input.GetAxis ("Horizontal") * movementSpeed;
+			movementDir.z = Input.GetAxis ("Vertical") * movementSpeed;
+
+			movementDir = transform.TransformDirection (movementDir);
+		}
+
+		Debug.Log (movementDir);
+		//Gravity is applied to the vertical axis
+		movementDir.y = movementDir.y - (gravity * Time.deltaTime);
+
+		//Player is moved
+		player.Move (movementDir * Time.deltaTime);
+		
 	}
 }
