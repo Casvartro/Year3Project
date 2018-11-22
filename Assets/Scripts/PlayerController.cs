6f29085d8d7edb2732d49a50bfe4075cae1ad614
Cieran Brazier <c.brazier@student.liverpool.ac.uk>
2018-11-22 00:30:53 +0000

    Player Development Day 2
    Fixed player movement in the direction by changing the X axis of the camera when the mouse moves rather than the player.
    Added in functionality for sprinting and crouching when the left shift or x keys are pressed (unable to use control in the unity editor).
    
    ---TO DO STILL---
    Fix movement where if a horizontal key and vertical movement key are pressed at the same time they do not have more speed.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float movementSpeed;
+	public float jumpSpeed;
+	public Vector3 crouchScale;
 	public float gravity = 20.0f;
-
+	public Transform cameraPos;
 	public float cameraSpeed;
+
 	private float rotY = 0.0f;
 	private float rotX = 0.0f;
+	private float playerSpeed = 0.0f;
+	private float playerJumpSpeed = 0.0f;
+	private Vector3 normalScale = new Vector3(1, 1, 1);
 
 	private Vector3 movementDir;
 	private CharacterController player;
@@ -17,6 +23,8 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		player = this.GetComponent<CharacterController> ();
+		cameraPos.position = new Vector3 (this.transform.position.x, this.transform.position.y - 0.5f,
+			this.transform.position.z);
 	}
 	
 	// Update is called once per frame
@@ -33,7 +41,9 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		//Euler angles used to overwrite the rotation value each time it changes to its new current position.
-		transform.eulerAngles = new Vector3(rotY, rotX, 0.0f);
+		transform.rotation = Quaternion.Euler(0.0f, rotX, 0.0f);
+		//Rotates the camera view rather than the actual player to prevet player from trying to move vertically.
+		cameraPos.localRotation = Quaternion.Euler (rotY, 0.0f, 0.0f);
 
 	}
 
@@ -41,20 +51,49 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate () {
 
 		if (player.isGrounded) {
+
+			//Modifies the player speed based on keys pressed down.
+			if (!Input.GetButton ("Sprint") || !Input.GetButton("Crouch")) {
+				if (this.transform.localScale.y == crouchScale.y){
+					this.transform.localScale = normalScale;
+				}
+				playerSpeed = movementSpeed;
+				playerJumpSpeed = jumpSpeed;
+			}
+				
+			if (Input.GetButton ("Sprint")) {
+				playerSpeed = movementSpeed * 2;
+				playerJumpSpeed = jumpSpeed * 1.5f;
+			}
+
+			if (Input.GetButton("Crouch")){
+				if (this.transform.localScale.y != crouchScale.y){
+					this.transform.localScale = crouchScale;
+					this.transform.position = new Vector3 (this.transform.position.x, 
+						crouchScale.y, this.transform.position.z);
+				}
+				playerSpeed = movementSpeed - 0.5f;
+			}
+
 			//Updates based on player input.
 			movementDir = Vector3.zero;
-			movementDir.x = Input.GetAxis ("Horizontal") * movementSpeed;
-			movementDir.z = Input.GetAxis ("Vertical") * movementSpeed;
+			movementDir.x = Input.GetAxis ("Horizontal") * playerSpeed;
+			movementDir.z = Input.GetAxis ("Vertical") * playerSpeed;
 
 			movementDir = transform.TransformDirection (movementDir);
-		}
 
-		Debug.Log (movementDir);
+			if (Input.GetButton ("Jump")) {
+				movementDir.y = playerJumpSpeed;
+			}
+
+
+		}
+			
 		//Gravity is applied to the vertical axis
 		movementDir.y = movementDir.y - (gravity * Time.deltaTime);
-
 		//Player is moved
 		player.Move (movementDir * Time.deltaTime);
 		
 	}
+
 }
