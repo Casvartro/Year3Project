15c793c9844ecd8b2f71d7b904d175f169d72846
Cieran Brazier <c.brazier@student.liverpool.ac.uk>
2018-11-23 17:18:15 +0000

    Player Development  Day 3
    Added base weapon model as a rectangle. Placed it under camera hierachy and attatched a bullet spawn to it.
    Created a base form of projectile to spawn as well.
    If player runs out of ammo they cannot fire anymore.
    
    --TO DO STILL--
    Fix movement issue in previous update, make basic UI for player functionality so far.

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+	public float bulletSpeed;
+	public int maxAmmo;
+
+	private int currentAmmo;
+
+	// Use this for initialization
+	void Start () {
+		currentAmmo = maxAmmo;
+	}
+
+	void LateUpdate () {
+		if (currentAmmo != 0) {
+			if (Input.GetMouseButtonDown (0)) {
+				fire ();
+				currentAmmo = currentAmmo - 1;
+			}
+		}
+	}
+
+	void fire(){
+
+		var bullet = Instantiate (bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+		bullet.GetComponent<Rigidbody> ().velocity = bullet.transform.forward * bulletSpeed;
+		Destroy (bullet, 5.0f);
+	}
+}
