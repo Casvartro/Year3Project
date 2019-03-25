a06dcde8ce69cc9284093c94af019bbe9cb55427
Cieran Brazier <c.brazier@student.liverpool.ac.uk>
2019-03-25 19:31:17 +0000

    Settings Update
    Added a slider text value to the volume sliders.

--- Assets/Scripts/MenuScripts/SettingsMenu.cs
+++ Assets/Scripts/MenuScripts/SettingsMenu.cs
@@ -7,8 +7,10 @@ public class SettingsMenu : MonoBehaviour {
 
 	public Slider mSlider;
 	public AudioSource mSource;
+	public Text mText;
 	public Slider sSlider;
 	public AudioSource sSource;
+	public Text sText;
 
 	private float wantedMusicVolume;
 	private float wantedSoundFXVolume;
@@ -19,19 +21,23 @@ public class SettingsMenu : MonoBehaviour {
 			wantedMusicVolume = PlayerPrefs.GetFloat ("musicVolume");
 			mSlider.value = wantedMusicVolume;
 			mSource.volume = wantedMusicVolume;
+			mText.text = Mathf.Round(mSlider.value * 100).ToString();
 		} else {
 			mSlider.value = mSource.volume;
+			mText.text =  Mathf.Round(mSlider.value * 100).ToString ();
 		}
 
 		if (sSlider != null && PlayerPrefs.HasKey ("soundFXVolume")) {
 			wantedSoundFXVolume = PlayerPrefs.GetFloat ("soundFXVolume");
 			sSlider.value = wantedSoundFXVolume;
+			sText.text =  Mathf.Round(sSlider.value * 100).ToString ();
 			if (sSource != null) {
 				sSource.volume = wantedSoundFXVolume;
 			}
 		} else {
 			if (sSource != null) {
 				sSlider.value = sSource.volume;
+				sText.text = Mathf.Round(sSlider.value * 100).ToString ();
 			}
 		}
 
@@ -46,10 +52,12 @@ public class SettingsMenu : MonoBehaviour {
 
 	public void setMusicVolume(float volume){
 		mSource.volume = volume;
+		mText.text = Mathf.Round(volume * 100).ToString ();
 		PlayerPrefs.SetFloat ("musicVolume", volume);
 	}
 
 	public void setSoundFXVolume(float volume){
+		sText.text = Mathf.Round(volume * 100).ToString ();
 		if (sSource != null) {
 			sSource.volume = volume;
 		}
